22660c7e71f0a0da2826edd35157473a155b0c59
Gopinath Nelluri <9353385+gopinathnelluri@users.noreply.github.com>
2024-01-11 13:51:03 -0600

    Update InterceptHttpResponse.js

--- JavaScript/InterceptHttpResponse.js
+++ JavaScript/InterceptHttpResponse.js
@@ -57,3 +57,38 @@ window.XMLHttpRequest = function () {
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true);
 xhr.send();
+
+
+// 3 - test
+
+
+// Save the original XMLHttpRequest object
+const originalXHROpen = window.XMLHttpRequest.prototype.open;
+const originalXHRSend = window.XMLHttpRequest.prototype.send;
+
+// Override the open method of XMLHttpRequest
+window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+  // Add your logic here before the actual open method is called
+  console.log(`Intercepted XHR request to ${url} with method ${method}`);
+
+  // Call the original open method
+  originalXHROpen.apply(this, arguments);
+};
+
+// Override the send method of XMLHttpRequest
+window.XMLHttpRequest.prototype.send = function(data) {
+  // Save a reference to the XMLHttpRequest object
+  const xhr = this;
+
+  // Add your logic here before the actual send method is called
+  xhr.addEventListener('load', function() {
+    // This event is triggered when the response is received
+    console.log('Intercepted XHR response:', xhr.responseText);
+
+    // You can access the response text using xhr.responseText
+    // Do something with the response text here
+  });
+
+  // Call the original send method
+  originalXHRSend.apply(this, arguments);
+};
